5bf9bb22d51dfcd9174887a6062c574003b6ea14
Shridhar600 <65012105+Shridhar600@users.noreply.github.com>
2023-01-22 15:11:34 +0530

    Modules till files.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-
 const logg = require('./logger');
+const paths = require('path');
+const osmods = require('os');
+const fs = require('fs');
 
 function hello(name){
     console.log('Hello ' + name );
@@ -9,3 +11,25 @@ hello('Shridhar');
 
 logg.loggerr('Honey');
 
+// path module
+var pathblah = paths.parse(__filename);
+
+console.log(pathblah);
+
+//os module
+
+var TotalMemory = osmods.totalmem();
+var freeMemory = osmods.freemem();
+
+console.log(TotalMemory);
+console.log(freeMemory);
+
+//file modules;
+
+ fs.readdir('./', function(err,files){
+    if(err){
+        console.log(`Error:  ${err}`);
+    }else{
+        console.log(`Results: ${files}`)
+    }
+ }); 
